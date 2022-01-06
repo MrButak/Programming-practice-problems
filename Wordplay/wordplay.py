62da548a8a7a3af841e5d071d291f4c927a8ac1f
Matthew Spence <mspence5555@gmail.com>
2022-01-06 16:49:55 -0600

    completed 'What are all of the words that have all 5 vowels, in alphabetical order?'

--- Wordplay/wordplay.py
+++ Wordplay/wordplay.py
@@ -55,3 +55,47 @@ in_file = open("sowpods.txt", "r")
 # in_file.seek(0)
 
 # What are all of the words that have all 5 vowels, in any order?
+# for words in in_file:
+#     if("A" in words and "E" in words and "I" in words 
+#     and "O" in words and "U" in words):
+#         print(words,end="")
+# in_file.seek(0)
+
+# What are all of the words that have all 5 vowels, in alphabetical order?
+# def check_vowels(word):
+#     word_len = len(word) - 1 # -1 for escape character
+#     start_index = - 1
+#     vowel_list = []
+#     vowel_list_ol = []
+#     vowels = ["A", "E", "I", "O", "U"]
+
+#     for i in range(word_len):
+#         start_index += 1
+#         # check if 5 vowels in a row is still possible              
+#         if(word[i] == "A" and (word_len - start_index) < 4):
+#             return
+#         # append all vowels to a list
+#         elif(word[i] == "A" or word[i] == "E" or word[i] == "I" or 
+#         word[i] == "O" or word[i] == "U"):
+#             vowel_list.append(word[i])
+
+#     # check if first vowel in list is 'A'
+#     if(vowel_list[0] != "A"):
+#         return
+#     # remove duplicates from list
+#     for i in vowel_list:
+#         if i not in vowel_list_ol:
+#             vowel_list_ol.append(i)
+#     # check if all vowels in a row
+#     for i in range(len(vowels)):
+#         if(vowel_list_ol[i] != vowels[i]):
+#             return
+#     # if all checks passed print the word
+#     print(word, end="")
+    
+# for words in in_file:
+#     # check if all vowels are in words
+#     if("A" in words and "E" in words and "I" in words 
+#     and "O" in words and "U" in words):
+#         check_vowels(words)
+# in_file.seek(0)
